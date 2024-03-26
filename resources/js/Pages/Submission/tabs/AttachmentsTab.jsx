3b06fb455ec8c2b09c2c5ccfa1798c03fe15cd8c
Francis Kamande <franciskamande2001@gmail.com>
2024-03-27 02:08:51 +0300

    Refactor AttachmentsTab component

--- resources/js/Pages/Submission/tabs/AttachmentsTab.jsx
+++ resources/js/Pages/Submission/tabs/AttachmentsTab.jsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 
 const AttachmentsTab = ({ documents }) => {
     const [selectedDocuments, setSelectedDocuments] = useState([]);
-    console.log('Selected Attachment: ',selectedDocuments);
-    console.log('Documents: ',documents);
+    console.log('Selected Attachment: ', selectedDocuments);
+    console.log('Documents: ', documents);
     const handleCheckboxChange = (documentId) => {
         if (selectedDocuments.includes(documentId)) {
             setSelectedDocuments(selectedDocuments.filter(id => id !== documentId));
@@ -20,13 +20,14 @@ const AttachmentsTab = ({ documents }) => {
                     <thead className="bg-gray-50">
                         <tr>
                             <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                <input 
-                                    type="checkbox" 
-                                    className="form-checkbox h-4 w-4 text-indigo-600" 
+                                <input
+                                    type="checkbox"
+                                    className="form-checkbox h-4 w-4 text-indigo-600"
                                     checked={selectedDocuments.length === documents.length}
-                                    onChange={(e) => { (e.target.checked) ? setSelectedDocuments(documents.map(document => document.id))
+                                    onChange={(e) => {
+                                        (e.target.checked) ? setSelectedDocuments(documents.map(document => document.id))
                                         : setSelectedDocuments([])
-                                        }
+                                    }
                                     }
                                 />
                             </th>
@@ -37,10 +38,10 @@ const AttachmentsTab = ({ documents }) => {
                                 Extension
                             </th>
                             <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Size (Bytes)
+                                Size
                             </th>
                             <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Status
+                                Action
                             </th>
                         </tr>
                     </thead>
@@ -48,9 +49,9 @@ const AttachmentsTab = ({ documents }) => {
                         {documents.map((document, index) => (
                             <tr key={index}>
                                 <td className="px-6 py-4 whitespace-nowrap">
-                                    <input 
-                                        type="checkbox" 
-                                        className="form-checkbox h-4 w-4 text-indigo-600" 
+                                    <input
+                                        type="checkbox"
+                                        className="form-checkbox h-4 w-4 text-indigo-600"
                                         checked={selectedDocuments.includes(document.id)}
                                         onChange={() => handleCheckboxChange(document.id)}
                                     />
@@ -62,10 +63,16 @@ const AttachmentsTab = ({ documents }) => {
                                     {document.extension}
                                 </td>
                                 <td className="px-6 py-4 whitespace-nowrap">
-                                    {document.size}
+                                    {document.size > 1024 ? `${(document.size / 1024).toFixed(2)} KB` : `${document.size} B`}
                                 </td>
                                 <td className="px-6 py-4 whitespace-nowrap">
-                                    {document.status}
+                                    <a href={`https://storage.cloud.google.com/manuscript-manager/manuscript-manager/${document.path}`} download={document.name}>
+                                        <button
+                                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                                        >
+                                            Download
+                                        </button>
+                                    </a>
                                 </td>
                             </tr>
                         ))}
